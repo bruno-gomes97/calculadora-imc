407672ff073aa21a5f52127c1459d24d979b5ed1
Bruno Gomes <gomes_bruno97@hotmail.com>
2024-10-16 17:48:23 -0300

    validação do formulário e reponsividade

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,12 +3,14 @@ import './MainContent.css'
 
 const MainContent = () => {
 
+    // Gerenciamento de estado
     const [userName, setUserName] = useState('');
     const [userHeight, setUserHeight] = useState('');
     const [userWeight, setUserWeight] = useState('');
     const [imc, setImc] = useState(null);
     const [showResult, setShowResult] = useState(false);
 
+    // Manipulação de inputs
     const handleNameChange = (e) => {
         setUserName(e.target.value);
     }
@@ -23,11 +25,22 @@ const MainContent = () => {
 
     const calculateIMC = (e) => {
         e.preventDefault();
-        const calculatedIMC = userWeight / (userHeight * userHeight);
-        setImc(calculatedIMC.toFixed(2));
-        setShowResult(true);
+
+        // Converte para número
+        const height = parseFloat(userHeight);
+        const weight = parseFloat(userWeight);
+
+        // Validar formulário
+        if (!isNaN(height) && !isNaN(weight) && height > 0 && weight > 0) {
+            const calculatedIMC = userWeight / (userHeight * userHeight);
+            setImc(calculatedIMC.toFixed(2));
+            setShowResult(true);
+        } else {
+            alert("Por favor, insira valores válidos para altura e peso!");
+        }
     }
 
+    // Limpar campos
     const clearInput = () => {
         setUserName('');
         setUserHeight('');
@@ -41,21 +54,21 @@ const MainContent = () => {
             <form>
                 <label htmlFor="name">Nome:</label>
                 <input type="text"
-                       value={userName}
-                       onChange={handleNameChange}
-                       placeholder="Digite o seu nome" />
+                    value={userName}
+                    onChange={handleNameChange}
+                    placeholder="Digite o seu nome" />
                 <label htmlFor="name">Altura:</label>
                 <input type="number"
-                       step={0.01}
-                       value={userHeight}
-                       onChange={handleHeightChange}
-                       placeholder="Digite sua altura ex.: 1.70" />
+                    step={0.01}
+                    value={userHeight}
+                    onChange={handleHeightChange}
+                    placeholder="Digite sua altura ex.: 1.70" />
                 <label htmlFor="name">Peso:</label>
                 <input type="number"
-                       step={0.01}
-                       value={userWeight}
-                       onChange={handleWeightChange}
-                       placeholder="Digite seu peso ex.: 60.5"  />
+                    step={0.01}
+                    value={userWeight}
+                    onChange={handleWeightChange}
+                    placeholder="Digite seu peso ex.: 60.5" />
                 <button type="submit" onClick={calculateIMC}>Calcular</button>
                 <button type="reset" onClick={clearInput}>Limpar</button>
             </form>
